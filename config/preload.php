b55d90b66c6ca45e4a9eeb4a1351038e1904ca72
Melody Kimball <melody@forestkimball.com>
2024-11-05 10:36:40 -0500

    Removed symfony/flex

--- config/preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(dirname(__DIR__) . '/var/cache/prod/Brightspace_Api_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__) . '/var/cache/prod/Brightspace_Api_KernelProdContainer.preload.php';
-}
